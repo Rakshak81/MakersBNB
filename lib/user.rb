6aa7d79539617766490e12150aa71c445e2ca7f1
Eddie Arnold <eddiearnoldz@me.com>
2022-02-21 19:33:55 +0000

    passed the tests for the identify method to allow signed up users to login and be ytaken to correct page.

--- lib/user.rb
+++ lib/user.rb
@@ -17,13 +17,11 @@ attr_reader :id, :username, :email, :password
     else
       connection = PG.connect(dbname: 'makersbnb_manager')
     end
-    p "SEEEE THIS"
-    p result = connection.params(
-      "SELECT * FROM users WHERE username = '#{username}' AND password = '#{password}'").first
-      if result == nil
-        User.new(id: result['id'], username: result['username'], email: result['email'], password: result['password]']) 
-      else
-        raise "Username or password incorrect"
+    result = connection.exec(
+      "SELECT * FROM users WHERE username='#{username}' AND password='#{password}';").first
+      if result
+      User.new(id: result['id'], username: result['username'], email: result['email'], password: result['password'])
+      else nil
       end
   end
 
@@ -36,7 +34,7 @@ attr_reader :id, :username, :email, :password
     result = connection.exec(
       "INSERT INTO users (username, email, password)
     VALUES('#{username}', '#{email}', '#{password}') RETURNING id, username, email, password;").first
-      User.new(id: result['id'], username: result['username'], email: result['email'], password: result['password]'])
+      User.new(id: result['id'], username: result['username'], email: result['email'], password: result['password'])
   end
 
   
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -14,9 +14,9 @@ describe User do
     it "checks the database for the user with the specified username and password" do
       user = User.create(username: 'eddiearnold', email: 'eddiearnold@me.com', password: '12345')
       identified_user = User.identify(username: user.username, password: user.password)
-      expect(identified_user.username).to eq 'eddiearnold'
+      expect(identified_user.username).to eq user.username
       expect(identified_user.id).to eq user.id
-      expect(identified_user.password).to eq '12345'
+      expect(identified_user.password).to eq user.password
     end
   end
 end
