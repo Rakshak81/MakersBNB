e3b87f705d302151188b3ac96ea4e391ed408a7f
Charlotte Buchan <cebuc@talktalk.net>
2022-02-22 12:17:00 +0000

    finish adding sessions page

--- app.rb
+++ app.rb
@@ -17,11 +17,11 @@ class Makersbnb < Sinatra::Base
     @user = User.create(email: params['email'], username: params['username'], password: params['password'])
     session[:user_id] = @user.id
     session[:user_name] = @user.username
-    redirect '/spaces'
+    redirect '/spaces/index'
   end
 
-  get '/spaces' do
-    erb :spaces
+  get '/spaces/index' do
+    erb :'spaces/index'
   end
 
   get '/sessions/new' do
@@ -29,14 +29,26 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/sessions/confirm' do
-    @user = User.identify(username: params['username'], password: params['passowrd'])
+
+    @user = User.identify(username: params['username'], password: params['password'])
    if @user 
-      redirect '/spaces'
+    session[:user_id] = @user.id
+    session[:username] = @user.username
+      redirect '/spaces/index'
    else
     redirect '/'
    end
   end
 
+  get '/spaces/new' do
+    erb :'spaces/new'
+  end
+
+post '/spaces/new' do
+  user = session[:user_id]
+  @space = Space.create(name: params['name'], description: params['description'], price: params['price'], start_date: params['start_date'], end_date: params['end_date'], user_id: user)
+  redirect '/spaces/index'
+end
 # get '/spaces' do
 #     erb :spaces
 #   end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -34,3 +34,32 @@ feature 'homepage' do
 
   end
 end
+
+feature 'spaces' do
+  scenario 'visit spaces page' do
+    visit '/spaces'
+    # expect(page).to have_content "sunny camping site"
+    # expect(page).to have_content "a sunny field to spend your holiday"
+    expect(page).to have_selector(:link_or_button, 'sign out')
+    expect(page).to have_selector(:link_or_button, 'list a space')
+    expect(page).to have_selector(:link_or_button, 'list available spaces')
+    expect(page).to have_field 'available from'
+    expect(page).to have_field 'available to'
+
+  end
+end
+feature 'list a space' do
+  scenario 'a user a space to the spaces table' do 
+    visit '/spaces/new'
+    fill_in('name', with: "house")
+    fill_in('description', with: 'fancy home')
+    fill_in('price', with: '100')
+    fill_in('start_date', with: '2022-02-22')
+    fill_in('end_date', with: '2023-02-27')
+    click_button 'list my space'
+    expect(page).to have_current_path '/spaces'
+    expect(page).to have_content "house"
+    expect(page).to have_content 'fancy home'
+
+  end
+end
