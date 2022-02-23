53e74a91a3967b91238f26f60d069fb13bec665c
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-23 13:46:58 +0000

    Added space request and confirmation functionality.

--- lib/space.rb
+++ lib/space.rb
@@ -1,5 +1,5 @@
 class Space
-  attr_reader :name, :description, :price, :start_date, :end_date, :user_id, :id
+  attr_reader :name, :description, :price, :start_date, :end_date, :user_id, :id, :request, :confirm
 
 
   def initialize(id:, name:, description:, price:, start_date:, end_date:, user_id:)
@@ -10,6 +10,8 @@ class Space
   @start_date = start_date
   @end_date = end_date
   @user_id = user_id
+  @request = false
+  @confirm = false
   end
 
   def self.create(name:, description:, price:, start_date:, end_date:, user_id:)
@@ -46,10 +48,46 @@ class Space
       connection = PG.connect(dbname: 'makersbnb_manager')
     end
     result = connection.exec("SELECT * FROM spaces
-      WHERE start_date >= '#{available_from}'")
+      WHERE start_date BETWEEN 2022-02-22 and 2022-02-25").first
     result.map do |space|
       Space.new(id: space['id'], name: space['name'], description: space['description'], price: space['price'],
         start_date: space['start_date'], end_date: space['end_date'], user_id: space['user_id'])
     end
+  end
+
+  def self.request(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_manager_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb_manager')
+    end
+  connection.exec_params("UPDATE spaces SET requested = TRUE
+    WHERE id = $1;", [id])
+  end
+end
+
+def self.requests_made(requested_by_id:)
+  if ENV['ENVIRONMENT'] == 'test'
+    connection = PG.connect(dbname: 'makersbnb_manager_test')
+  else
+    connection = PG.connect(dbname: 'makersbnb_manager')
+  end
+result = connection.exec("SELECT * FROM spaces WHERE requested_by_id = '#{requested_by_id}' and requested = true")
+result.map do |space|
+  Space.new(id: space['id'], name: space['name'], description: space['description'], price: space['price'],
+    start_date: space['start_date'], end_date: space['end_date'], user_id: space['user_id'])
+  end
+end
+
+def self.requests_to_confirm(user_id:)
+  if ENV['ENVIRONMENT'] == 'test'
+    connection = PG.connect(dbname: 'makersbnb_manager_test')
+  else
+    connection = PG.connect(dbname: 'makersbnb_manager')
+  end
+result = connection.exec("SELECT * FROM spaces WHERE user_id = '#{user_id}' and requested = true")
+result.map do |space|
+  Space.new(id: space['id'], name: space['name'], description: space['description'], price: space['price'],
+    start_date: space['start_date'], end_date: space['end_date'], user_id: space['user_id'])
   end
 end
